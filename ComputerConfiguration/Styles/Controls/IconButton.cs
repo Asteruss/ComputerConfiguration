be92982d1cbfe1041b28addb37d689ca3d6f483b
Asteruss <artyom.yurchenko2222@mail.ru>
2026-05-19 17:17:52 +0300

    IconButton + кнопки сортировок

---
+++ ComputerConfiguration/Styles/Controls/IconButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ComputerConfiguration.Styles.Controls;
+
+public class IconButton : Button
+{
+    public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+        nameof(Icon), typeof(ImageSource), typeof(IconButton)
+        );
+
+    public static readonly DependencyProperty SizeProperty = DependencyProperty.Register(
+        nameof(Size), typeof(double), typeof(IconButton)
+        );
+
+    public ImageSource Icon
+    {
+        get => (ImageSource)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public double Size
+    {
+        get => (double)GetValue(SizeProperty);
+        set => SetValue(SizeProperty, value);
+    }
+}
